7c52ce46a71c4c014443f4b71c89d888f310dd3c
Tomorrow Website <94855523+tomorrowWebsite@users.noreply.github.com>
2022-01-11 07:03:28 +0200

    go

---
+++ Building-app.js
@@ -0,0 +1,38 @@
+// 1 vue create file-name
+// 2 choose Manually select features
+// ---- Choose Vue Version
+// ---- Bable
+// ---- Router
+// ---- Vuex
+// ---- Css Pre-Processors
+// ---- Linter Formatter
+// ---- Unit Testing
+// --------- 3.x
+// --------- y
+// --------- Sass/Scss (With node-sass)
+// --------- Eslint + prettier
+// --------- Lint on save
+// --------- jest
+// --------- In dedicated config files
+// --------- y
+// --------- vue-cli-3-custom
+// 3 npm run serve
+// 4 npm run lint
+// 5 npm run build
+// 6 npm install -g serve
+// 7 serve -s dist
+// 8 firebase init
+// ---- y
+// ----  ( ) Hosting: Configure files for Firebase Hosting and (optionally) set up GitHub Action deploys
+// ---- dist
+// ---- y
+// ---- y
+// ---- n
+// ---- tomorrowWebsite/repositoryName
+// ---- y
+// ---- npm run build
+// ---- y
+// ---- y
+// ---- main
+// ---- y
+// ---- firebase deploy --only hosting
